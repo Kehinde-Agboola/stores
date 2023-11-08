51caa5f5cc677c82a6ab40cee8a69f06d35cc513
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-11-08 22:40:20 +0100

    updated-total

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/products.models';
+import { CartService } from 'src/app/services/cart.service';
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
@@ -10,11 +12,23 @@ export class HomeComponent {
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
+  constructor(private cartService: CartService) {}
+
   onColumsCountChange(colsNum: number) {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols];
   }
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id,
+    });
   }
 }
