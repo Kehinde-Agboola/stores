54dd7bb1bc63eb4bb6d7e70d93e877dbd443079e
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-11-08 22:39:07 +0100

    update-cart-badge

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
 
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+  constructor(private CartService: CartService) {}
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.CartService.getTotal(items);
+  }
+  onClearCart(): void {
+    this.CartService.clearCart();
+  }
 }
