aa5f93c9176d0da453a035bef650fe26f47aa683
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-11-08 22:40:36 +0100

    init

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  constructor(private CartService: CartService) {}
   cart: Cart = {
     items: [
+      {
+        product: 'https://placehold.co/400/orange/white',
+        name: 'sneakers',
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: 'https://placehold.co/400/orange/white',
+        name: 'sneakers',
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
       {
         product: 'https://placehold.co/400/orange/white',
         name: 'sneakers',
@@ -22,14 +38,17 @@ export class CartComponent {
 
   displayedColumns: Array<string> = [
     'product',
-    // 'name',
-    // 'price',
-    // 'quantity',
-    // 'total',
-    // 'action',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
   ];
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+  getTotal(items: Array<CartItem>): number {
+    return this.CartService.getTotal(items);
   }
 }
