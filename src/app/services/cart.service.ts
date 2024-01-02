0fb0c8d527c91da1b75ba97deb52ac664c8aea65
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-01-03 00:10:46 +0100

    cart-made-dynamic

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Cart, CartItem } from '../models/cart.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Cart, CartItem } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,25 +14,66 @@ export class CartService {
   addToCart(item: CartItem): void {
     const items = [...this.cart.value.items];
 
-    const itemsInCart = items.find((_item) => _item.id === item.id);
-
-    if (itemsInCart) {
-      itemsInCart.quantity += 1;
+    const itemInCart = items.find((_item) => _item.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity += 1;
     } else {
       items.push(item);
     }
+
     this.cart.next({ items });
-    this._snackBar.open('1 item added to cart', 'OK', { duration: 3000 });
-    console.log(this.cart.value);
+    this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
   }
-  getTotal(items: Array<CartItem>): number {
-    return items
-      .map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+
+  removeFromCart(item: CartItem, updateCart = true): CartItem[] {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (updateCart) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 item removed from cart.', 'Ok', {
+        duration: 3000,
+      });
+    }
+
+    return filteredItems;
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval!: CartItem;
+
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+
+      return _item;
+    });
+
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
+    }
+
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open('1 item removed from cart.', 'Ok', {
+      duration: 3000,
+    });
   }
 
   clearCart(): void {
     this.cart.next({ items: [] });
-    this._snackBar.open('1 cart is removed.', 'OK', { duration: 3000 });
+    this._snackBar.open('Cart is cleared.', 'Ok', {
+      duration: 3000,
+    });
+  }
+
+  getTotal(items: CartItem[]): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
